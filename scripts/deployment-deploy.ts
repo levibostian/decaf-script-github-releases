4d5fc18b3a23dec59dd387ea423f99f4692137a9
Levi Bostian <levi@levi.earth>
2025-10-09 07:28:01 -0500

    feat: publish binaries to github releases as a way to run this tool

--- scripts/deployment-deploy.ts
+++ scripts/deployment-deploy.ts
@@ -5,8 +5,38 @@ import { getDeployStepInput } from "@levibostian/decaf-sdk"
 
 const input = getDeployStepInput()
 
-// Deno publish 
+const githubReleaseAssets: string[] = []
 
+const compileBinary = async ({ denoTarget, outputFileName }: { denoTarget: string; outputFileName: string }) => {
+  // Create dist directory if it doesn't exist
+  await $`mkdir -p dist`.printCommand()
+  await $`OUTPUT_FILE_NAME=dist/${outputFileName} DENO_TARGET=${denoTarget} deno task compile`.printCommand()
+
+  githubReleaseAssets.push(`dist/${outputFileName}#${outputFileName}`)
+}
+
+// Compile binaries for different platforms
+await compileBinary({
+  denoTarget: "x86_64-unknown-linux-gnu",
+  outputFileName: "bin-x86_64-Linux",
+})
+
+await compileBinary({
+  denoTarget: "aarch64-unknown-linux-gnu",
+  outputFileName: "bin-aarch64-Linux",
+})
+
+await compileBinary({
+  denoTarget: "x86_64-apple-darwin",
+  outputFileName: "bin-x86_64-Darwin",
+})
+
+await compileBinary({
+  denoTarget: "aarch64-apple-darwin",
+  outputFileName: "bin-aarch64-Darwin",
+})
+
+// Publish the deno module to jsr
 const argsToDenoPublish = [
   "publish",
   "--set-version",
@@ -21,8 +51,7 @@ if (input.testMode) {
 // https://github.com/dsherret/dax#providing-arguments-to-a-command
 await $`deno ${argsToDenoPublish}`.printCommand()
 
-// GitHub Release 
-
+// GitHub Release with binaries
 const argsToCreateGithubRelease = [
   `release`,
   `create`,
@@ -30,14 +59,13 @@ const argsToCreateGithubRelease = [
   `--generate-notes`,
   `--latest`,
   `--target`,
-  'main'
+  'main',
+  ...githubReleaseAssets,
 ]
 
 if (input.testMode) {
   console.log("Running in test mode, skipping creating GitHub release.")
   console.log(`Command to create GitHub release: gh ${argsToCreateGithubRelease.join(" ")}`)
-
-  Deno.exit(0)
+} else {
+  await $`gh ${argsToCreateGithubRelease}`.printCommand()
 }
-
-await $`gh ${argsToCreateGithubRelease}`.printCommand()
