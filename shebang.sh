5051a086205aaf12ad6843c55d7d85ac8b689eb1
Levi Bostian <levi@levi.earth>
2026-05-31 06:39:53 -0500

    prepare for shebang using mise

--- shebang.sh
+++ shebang.sh
@@ -1,17 +1,4 @@
 #!/usr/bin/env bash
-set -eu
 
-# Script that runs via decaf's shebang feature.
+mise exec -- deno task run "$@"
 
-REPO="levibostian/decaf-script-github-releases"
-
-if [ "$DECAF_SHEBANG_REF" = "tag" ]; then
-  # if shebang ran for a tag, download pre-built binary and run it. 
-  URL="https://github.com/$REPO/releases/download/$DECAF_SHEBANG_REF_NAME/install.sh"
-
-  curl -fsSL "$URL" | sh
-  $HOME/.local/bin/decaf-script-github-releases "$@"
-else
-  # else, run via deno. 
-  deno task --quiet run "$@"
-fi
